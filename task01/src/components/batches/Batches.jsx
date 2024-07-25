e52e1a7d35005bb2bb6f4c40716cbb5e0db2970e
Samiran_Das <samiran4209@gmail.com>
2024-07-26 01:56:08 +0530

    ""

--- task01/src/components/batches/Batches.jsx
+++ task01/src/components/batches/Batches.jsx
@@ -108,6 +108,19 @@ const filterData=data.filter((item)=>item.title.toLowerCase().includes(searchQue
           </div>
         </section>
       </div>
+      <img
+        src='https://s3-alpha-sig.figma.com/img/6dbf/e4f9/9eddf1549be82b67d870f4041b254cab?Expires=1722816000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Et6vIZoTixOW93hRDSTMTP5zHiXBScPptDa7wio9Q19Fb3RVeyKyGSdMZNMc8b03m8cZ0ujW0IgAcOHc5a5pATb6wYlKbIcOCU3CVwOpaS5a40VH89QQ~eBtGj5qfiC9d6yfNL4gcOFGfWUKDtlz4flPXQaJAMOUP~rft27nkvk7Cbinif4IiEllm4khAfpzXqTNh48H8JOUsSgdQXBHIkL12OEZd~XdmNdfnl6lLF4M-69ZTRv7nip6jGr6zKiQ6qpV5P~BzFPPLDw0PZWjV~zQmnt8eRGvdRSuyjK9KlUjSNaNyVi8P2eeXixyUAJDCmEVW6CB6SozO0auNevxjQ__'
+        alt='Background'
+        style={{
+          position: 'absolute',
+          right: '10px',
+          bottom: '10px',
+          width: '100px',
+          height: '100px',
+          borderRadius: '18px',
+          zIndex: 10 // Ensure it's above other content
+        }}
+      />
     </div>
   );
 }
